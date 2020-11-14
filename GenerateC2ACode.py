984ae11dd11bfd67aa8f3bbcb53eae95102820b2
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2020-11-14 17:10:15 +0900

    add code of tlm def

--- GenerateC2ACode.py
+++ GenerateC2ACode.py
@@ -8,8 +8,8 @@ import csv
 import os
 import re           # 正規表現
 import pprint
+import sys
 
-# import sys
 # import os
 # import os.path
 # import msvcrt               # Enter不要な入力用
@@ -94,20 +94,24 @@ def GenerateCmdDef(settings, sgc_db):
     # "  cmd_table_[Cmd_CODE_NOP].cmd_func = Cmd_NOP;"
     # "  Cmd_CODE_NOP = 0x0000,"
     for i in range(DATA_SART_ROW, len(sgc_db)):
-        if sgc_db[i][0] != "":                                 # Comment
-            continue
-        if sgc_db[i][0] == "" and sgc_db[i][1] == "":         # CommentもNameも空白なら打ち切り
+        comment = sgc_db[i][0]
+        name    = sgc_db[i][1]
+        cmd_id  = sgc_db[i][3]
+        if comment == "" and name == "":                  # CommentもNameも空白なら打ち切り
             break
-        # print(sgc_db[i][1])
-        cmd_name = sgc_db[i][1]
+        if comment != "":                                 # Comment
+            continue
+        # print(name)
+        cmd_name = name
         cmd_code = cmd_name.replace("Cmd_", "Cmd_CODE_")
         # print(cmd_name)
         # print(cmd_code)
         body_c += "  cmd_table_[" + cmd_code + "].cmd_func = " + cmd_name + ";\n"
-        body_h += "  " + cmd_code + " = " + sgc_db[i][3] + ",\n"
+        body_h += "  " + cmd_code + " = " + cmd_id + ",\n"
 
     OutputCmdDefC(output_file_path + output_file_name_base + ".c", body_c)
     OutputCmdDefH(output_file_path + output_file_name_base + ".h", body_h)
+
 
 def GenerateBctDef(settings, bct_db):
     output_file_path = settings["c2a_root_dir"] + r"src_user/CmdTlm/"
@@ -138,7 +142,95 @@ def GenerateBctDef(settings, bct_db):
 
 
 def GenerateTlmDef(settings, tlm_db):
-    pass
+    output_file_path = settings["c2a_root_dir"] + r"src_user/CmdTlm/"
+    output_file_name_base = "TelemetryDefinitions"
+
+    DATA_SART_ROW = 7
+
+    body_c = ""
+    body_h = ""
+
+    # "static int OBC_(unsigned char* contents, int max_len);"
+    # "  OBC_ID = 0x00,"
+    for tlm in tlm_db:
+        body_c += "static int " + tlm['tlm_name'] + "_(unsigned char* contents, int max_len);\n"
+        body_h += "  " + tlm['tlm_name'] + "_ID = " + tlm['tlm_id'] + ",\n"
+
+    body_c += "\n"
+    body_c += "void TF_load_tlm_table(TlmInfo tlm_table_[TLM_MAX_TLMS])\n"
+    body_c += "{\n"
+    for tlm in tlm_db:
+        # "  tlm_table_[OBC_ID].tlm_func = OBC_;"
+        body_c += "  tlm_table_[" + tlm['tlm_name'] + "_ID].tlm_func = " + tlm['tlm_name'] + "_;\n"
+    body_c += "}\n"
+
+    for tlm in tlm_db:
+        func_code = ""
+        max_pos = ""
+        for i in range(DATA_SART_ROW, len(tlm['data'])):
+            comment  = tlm['data'][i][0]
+            name     = tlm['data'][i][1]
+            var_type = tlm['data'][i][2]
+            code     = tlm['data'][i][3]
+            pos      = tlm['data'][i][4]
+            if comment == "" and name == "":                    # CommentもNameも空白なら打ち切り
+                break
+            if comment != "":
+                continue
+            if name == "":
+                continue
+            if var_type == "":
+                continue
+            if code == "":
+                continue
+            if pos == "":
+                continue
+
+            code = code.replace("@@", ",")
+            func_code += "  "
+            if   var_type == "int8_t":
+                func_code += "copy_i8_"
+                max_pos = int(pos) + 1
+            elif var_type == "int16_t":
+                func_code += "copy_i16_"
+                max_pos = int(pos) + 2
+            elif var_type == "int32_t":
+                func_code += "copy_i32_"
+                max_pos = int(pos) + 4
+            elif var_type == "uint8_t":
+                func_code += "copy_u8_"
+                max_pos = int(pos) + 1
+            elif var_type == "uint16_t":
+                func_code += "copy_u16_"
+                max_pos = int(pos) + 2
+            elif var_type == "uint32_t":
+                func_code += "copy_u32_"
+                max_pos = int(pos) + 4
+            elif var_type == "float":
+                func_code += "copy_float_"
+                max_pos = int(pos) + 4
+            elif var_type == "double":
+                func_code += "copy_double_"
+                max_pos = int(pos) + 8
+            else:
+                print("Error: Tlm DB Err at " + tlm['tlm_name'], file=sys.stderr)
+                sys.exit(1)
+            func_code += "(&contents[" + pos + "], " + code + ");\n"
+
+        body_c += "\n"
+        body_c += "static int " + tlm['tlm_name'] + "_(unsigned char* contents, int max_len)\n"
+        body_c += "{\n"
+        body_c += "\n"
+        body_c += "  if( " + str(max_pos) + " > max_len) { return TLM_TOO_SHORT_LEN; }\n"
+        body_c += "\n"
+        body_c += func_code
+        body_c += "\n"
+        body_c += "  return " + str(max_pos) + ";\n"
+        body_c += "}\n"
+
+    OutputTlmDefC(output_file_path + output_file_name_base + ".c", body_c)
+    OutputTlmDefH(output_file_path + output_file_name_base + ".h", body_h)
+
 
 
 def OutputCmdDefC(file_path, body):
@@ -261,6 +353,95 @@ void BC_load_defaults(void);
 
     with open(file_path, mode='w', encoding='shift_jis') as fh:
         fh.write(output)
+
+
+def OutputTlmDefC(file_path, body):
+    output = ""
+    output += '''
+#pragma section REPRO
+/**
+ * @file   TelemetryDefinitions.c
+ * @brief  テレメトリ定義
+ * @author 鈴本 遼
+ * @date   2020/08/23
+ */
+#include "TelemetryDefinitions.h"
+#include "TelemetrySource.h"
+
+/*
+This pattern is a "separator".
+This should not be changed.
+This should not be used in other places.
+*/
+//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##
+'''[1:]         # 最初の改行を除く
+
+    output += body
+
+    output += '''
+
+//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##
+/*
+This pattern is a "separator".
+This should not be changed.
+This should not be used in other places.
+*/
+
+#pragma section
+'''[1:]         # 最初の改行を除く
+
+
+    with open(file_path, mode='w', encoding='shift_jis') as fh:
+        fh.write(output)
+
+
+def OutputTlmDefH(file_path, body):
+    output = ""
+    output += '''
+/**
+ * @file   TelemetryDefinitions.h
+ * @brief  テレメトリ定義
+ * @author 鈴本 遼
+ * @date   2020/08/23
+ */
+#ifndef TELEMETRY_DEFINITIONS_H_
+#define TELEMETRY_DEFINITIONS_H_
+
+#include "../../src_core/CmdTlm/TelemetryFrame.h"
+
+typedef enum
+{
+/*
+This pattern is a "separator".
+This should not be changed.
+This should not be used in other places.
+*/
+//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##
+
+'''[1:]         # 最初の改行を除く
+
+    output += body
+
+    output += '''
+
+//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##//##
+/*
+This pattern is a "separator".
+This should not be changed.
+This should not be used in other places.
+*/
+  TLM_ID_MAX
+} TLMID;
+
+void TF_load_tlm_table(TlmInfo tlm_table_[TLM_MAX_TLMS]);
+
+
+#endif // TLM_FRAME_INCLUDE
+'''[1:]         # 最初の改行を除く
+
+    with open(file_path, mode='w', encoding='shift_jis') as fh:
+        fh.write(output)
+
 
 
 if __name__ == '__main__':
