eaff52b329ee869d463f6c02e3a20aa801c851fe
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2021-11-26 19:35:50 +0900

    add initial commit

---
+++ c2aenum/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .enum_loader import C2aEnum, load_enum
---
+++ c2aenum/enum_loader.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+class C2aEnum:
+    def __init__(self, path):
+        self.path = path
+
+        self._load_bc()
+        self._load_tlm_code()
+        self._load_cmd_code()
+        self._load_app()
+        self._load_anomaly()
+        self._load_mode()
+        self._load_exec_sts()
+        self._load_tl_id()
+
+    def _load_bc(self):
+        self._load_numbered_enum_from_file(
+            "/src_user/CmdTlm/block_command_definitions.h", "BC_"
+        )
+
+    def _load_tlm_code(self):
+        self._load_numbered_enum_from_file(
+            "/src_user/CmdTlm/telemetry_definitions.h", "Tlm_CODE_"
+        )
+
+    def _load_cmd_code(self):
+        self._load_numbered_enum_from_file(
+            "/src_user/CmdTlm/command_definitions.h", "Cmd_CODE_"
+        )
+
+    def _load_app(self):
+        pass
+
+    def _load_anomaly(self):
+        pass
+
+    def _load_mode(self):
+        pass
+
+    def _load_exec_sts(self):
+        self._load_numbered_enum_from_file(
+            "/src_core/CmdTlm/common_tlm_cmd_packet.h", "CCP_EXEC_"
+        )
+
+    def _load_tl_id(self):
+        self._load_numbered_enum_from_file(
+            "/src_core/Applications/timeline_command_dispatcher.h", "TL_ID_"
+        )
+
+    def _load_numbered_enum_from_file(self, path, prefix):
+        path = self.path + path
+        p = re.compile("  {}.* = .*,.*".format(prefix))
+
+        with open(path, encoding="shift_jis") as f:
+
+            for line in f.readlines():
+                m = p.match(line)
+                if not m:
+                    continue
+
+                # 最初の空白と後ろのカンマ・改行を捨てる
+                enum_name, enum_id = m.string[2:-2].split(" = ")
+                enum_id = enum_id.split(",")[0]
+
+                if enum_id[:2] == "0x":
+                    enum_id = int(enum_id, base=16)
+                else:
+                    enum_id = int(enum_id, base=10)
+
+                self.__setattr__(enum_name, enum_id)
+
+    def _load_non_numbered_enum_from_file(self, path, prefix):
+        # TBD
+        pass
+
+
+def load_enum(c2a_abs_path) -> C2aEnum:
+    c2a_enum = C2aEnum(c2a_abs_path)
+    return c2a_enum
+
+
+if __name__ == "__main__":
+    # 単なる動作確認用
+    c2a_enum = load_enum(os.path.dirname(__file__) + "/../../../c2a_user_oss")
