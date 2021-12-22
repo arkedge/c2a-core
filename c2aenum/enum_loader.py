604d85d7346c8b8fd885f256b27d407c31216962
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2021-12-23 02:10:35 +0900

    support for enums without id

--- c2aenum/enum_loader.py
+++ c2aenum/enum_loader.py
@@ -53,17 +53,24 @@ class C2aEnum:
 
     def _load_numbered_enum_from_file(self, path, prefix):
         path = self.path + path
-        p = re.compile(r"^  ({}\w+) = (\w+)".format(prefix))
+        p_with_id = re.compile(r"^  ({}\w+) = (\w+)".format(prefix))
+        p_without_id = re.compile(r"^  ({}\w+)".format(prefix))
 
         with open(path, encoding="shift_jis") as f:
 
+            last_enum_id = 0
             for line in f.readlines():
-                m = p.match(line)
-                if not m:
+                m_with_id = p_with_id.match(line)
+                m_without_id = p_without_id.match(line)
+                if not m_with_id and not m_without_id:
                     continue
 
-                enum_name = m.group(1)
-                enum_id   = m.group(2)
+                if m_with_id:
+                    enum_name = m_with_id.group(1)
+                    enum_id   = m_with_id.group(2)
+                else:
+                    enum_name = m_without_id.group(1)
+                    enum_id   = str(last_enum_id + 1)
 
                 if enum_id[:2] == "0x":
                     enum_id = int(enum_id, base=16)
@@ -71,6 +78,7 @@ class C2aEnum:
                     enum_id = int(enum_id, base=10)
 
                 self.__setattr__(enum_name, enum_id)
+                last_enum_id = enum_id
 
     def _load_non_numbered_enum_from_file(self, path, prefix):
         # TBD
