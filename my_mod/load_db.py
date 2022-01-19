547aebd6642a86cfdc63ba222f53aba076168eae
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-01-20 00:41:57 +0900

    apply black

--- my_mod/load_db.py
+++ my_mod/load_db.py
@@ -6,8 +6,10 @@ DB読み込み
 import os
 import sys
 import csv
-import re           # 正規表現
-import pprint
+import re  # 正規表現
+
+# import pprint
+
 
 def LoadCmdDb(settings):
     cmd_db_path = settings["c2a_root_dir"] + r"src_user/Settings/TlmCmd/DataBase/CMD_DB/"
@@ -16,23 +18,23 @@ def LoadCmdDb(settings):
 
     other_obc_dbs = {}
     if settings["is_main_obc"]:
-        other_obc_dbs = LoadOtherObcCmd_(settings);
+        other_obc_dbs = LoadOtherObcCmd_(settings)
 
     # TODO: 重複チェックをする
 
     # print(sgc_db)
     # print(bct_db)
-    return {'sgc': sgc_db, 'bct': bct_db, 'other_obc': other_obc_dbs}
+    return {"sgc": sgc_db, "bct": bct_db, "other_obc": other_obc_dbs}
 
 
 def LoadCmdCSV_(cmd_db_path, db_prefix):
-    sgc_db_path = cmd_db_path + db_prefix + "_CMD_DB_CMD_DB.csv"        # single cmd
-    bct_db_path = cmd_db_path + db_prefix + "_CMD_DB_BCT.csv"           # block cmd table
+    sgc_db_path = cmd_db_path + db_prefix + "_CMD_DB_CMD_DB.csv"  # single cmd
+    bct_db_path = cmd_db_path + db_prefix + "_CMD_DB_BCT.csv"  # block cmd table
 
-    with open(sgc_db_path, mode='r', encoding='shift_jis') as fh:
+    with open(sgc_db_path, mode="r", encoding="shift_jis") as fh:
         reader = csv.reader(fh)
         sgc_db = [row for row in reader]
-    with open(bct_db_path, mode='r', encoding='shift_jis') as fh:
+    with open(bct_db_path, mode="r", encoding="shift_jis") as fh:
         reader = csv.reader(fh)
         bct_db = [row for row in reader]
 
@@ -40,17 +42,19 @@ def LoadCmdCSV_(cmd_db_path, db_prefix):
 
 
 def LoadTlmDb(settings):
-    tlm_db_path = settings["c2a_root_dir"] + r"src_user/Settings/TlmCmd/DataBase/TLM_DB/calced_data/"
+    tlm_db_path = (
+        settings["c2a_root_dir"] + r"src_user/Settings/TlmCmd/DataBase/TLM_DB/calced_data/"
+    )
 
     tlm_db = LoadTlmCSV_(tlm_db_path, settings["db_prefix"], settings["tlm_id_range"])
 
     other_obc_dbs = {}
     if settings["is_main_obc"]:
-        other_obc_dbs = LoadOtherObcTlm(settings);
+        other_obc_dbs = LoadOtherObcTlm(settings)
 
     # TODO: 重複チェックをする
 
-    return {'tlm': tlm_db, 'other_obc': other_obc_dbs}
+    return {"tlm": tlm_db, "other_obc": other_obc_dbs}
 
 
 def LoadTlmCSV_(tlm_db_path, db_prefix, tlm_id_range):
@@ -65,30 +69,37 @@ def LoadTlmCSV_(tlm_db_path, db_prefix, tlm_id_range):
 
     for tlm_name in tlm_names:
         tlm_sheet_path = tlm_db_path + db_prefix + "_TLM_DB_" + tlm_name + ".csv"
-        with open(tlm_sheet_path, mode='r', encoding='shift_jis') as fh:
+        with open(tlm_sheet_path, mode="r", encoding="shift_jis") as fh:
             reader = csv.reader(fh)
             sheet = [row for row in reader]
             # pprint.pprint(sheet)
             # print(sheet)
-            tlm_id = sheet[1][2]        # FIXME: テレメIDを取得．マジックナンバーで指定してしまってる．
+            tlm_id = sheet[1][2]  # FIXME: テレメIDを取得．マジックナンバーで指定してしまってる．
             if not int(tlm_id_range[0], 0) <= int(tlm_id, 0) < int(tlm_id_range[1], 0):
-                print("Error: TLM ID is invalid at " + db_prefix + "_TLM_DB_" + tlm_name + ".csv", file=sys.stderr)
+                print(
+                    "Error: TLM ID is invalid at " + db_prefix + "_TLM_DB_" + tlm_name + ".csv",
+                    file=sys.stderr,
+                )
                 sys.exit(1)
-            enable_flag = sheet[2][2]        # FIXME: Enable/Disable を取得．マジックナンバーで指定してしまってる．
+            enable_flag = sheet[2][2]  # FIXME: Enable/Disable を取得．マジックナンバーで指定してしまってる．
             if enable_flag != "ENABLE":
                 continue
-            raw_local_vars =  sheet[1][3].replace("%%", "").split("##")     # FIXME: ローカル変数を取得．マジックナンバーで指定してしまってる．
+            raw_local_vars = (
+                sheet[1][3].replace("%%", "").split("##")
+            )  # FIXME: ローカル変数を取得．マジックナンバーで指定してしまってる．
             local_vars = []
             for raw_local_var in raw_local_vars:
                 local_var = raw_local_var.strip()
                 if len(local_var) > 0:
                     local_vars.append(local_var)
-            tlm_db.append({'tlm_id': tlm_id, 'tlm_name': tlm_name, 'local_vars': local_vars, 'data': sheet})
+            tlm_db.append(
+                {"tlm_id": tlm_id, "tlm_name": tlm_name, "local_vars": local_vars, "data": sheet}
+            )
             # tlm_db.append({'tlm_id': tlm_id, 'tlm_name': tlm_name, 'data': 1})
 
-    tlm_db.sort(key=lambda x: x['tlm_id'])
+    tlm_db.sort(key=lambda x: x["tlm_id"])
 
-    return tlm_db;
+    return tlm_db
 
 
 def LoadOtherObcCmd_(settings):
@@ -116,9 +127,12 @@ def LoadOtherObcTlm(settings):
             continue
         tlm_db_path = settings["other_obc_data"][i]["db_path"] + r"TLM_DB/calced_data/"
 
-        tlm_db = LoadTlmCSV_(tlm_db_path, settings["other_obc_data"][i]["db_prefix"], settings["other_obc_data"][i]["tlm_id_range"])
+        tlm_db = LoadTlmCSV_(
+            tlm_db_path,
+            settings["other_obc_data"][i]["db_prefix"],
+            settings["other_obc_data"][i]["tlm_id_range"],
+        )
         other_obc_dbs[settings["other_obc_data"][i]["name"]] = tlm_db
 
     # pprint.pprint(other_obc_dbs)
     return other_obc_dbs
-
--- my_mod/tlm_buffer.py
+++ my_mod/tlm_buffer.py
@@ -3,10 +3,11 @@
 tlm buffer
 """
 
-import os
 import sys
+
 # from collections import OrderedDict
-import pprint
+# import pprint
+
 
 def GenerateTlmBuffer(settings, other_obc_dbs):
     DATA_START_ROW = 8
@@ -33,32 +34,40 @@ def GenerateTlmBuffer(settings, other_obc_dbs):
         body_h += "typedef struct\n"
         body_h += "{{\n"
         for tlm in tlm_db:
-            tlm_name = tlm['tlm_name']
+            tlm_name = tlm["tlm_name"]
             tlm_name_lower = tlm_name.lower()
             body_h += "  struct\n"
             body_h += "  {{\n"
             body_h += "    int     size;\n"
             body_h += "    uint8_t buffer[{_obc_name_upper}_TELEMETRY_BUFFE_SIZE];\n"
             body_h += "  }} " + tlm_name_lower + ";\n"
-            body_c += "static DS_ERR_CODE {_obc_name_upper}_analyze_tlm_" + tlm_name_lower + "_(DS_StreamConfig* p_stream_config, " + driver_type + "* " + driver_name + ");\n"
+            body_c += (
+                "static DS_ERR_CODE {_obc_name_upper}_analyze_tlm_"
+                + tlm_name_lower
+                + "_(DS_StreamConfig* p_stream_config, "
+                + driver_type
+                + "* "
+                + driver_name
+                + ");\n"
+            )
 
         body_h += "}} {_obc_name_upper}_Buffer;\n"
         body_h += "\n"
         tlmdef_body_h += "typedef struct\n"
         tlmdef_body_h += "{{\n"
         for tlm in tlm_db:
-            tlm_name = tlm['tlm_name']
+            tlm_name = tlm["tlm_name"]
             tlm_name_lower = tlm_name.lower()
 
             # pprint.pprint(tlm['data'][DATA_START_ROW:])
             last_var_type = ""
-            tlm_struct_tree = {}                          # python3.7以上を想定しているので，キーの順番は保存されていることが前提
+            tlm_struct_tree = {}  # python3.7以上を想定しているので，キーの順番は保存されていることが前提
             # tlm_struct_tree = collections.OrderedDict()     # やっぱこっちで
-            for j in range(DATA_START_ROW, len(tlm['data'])):
-                comment  = tlm['data'][j][0]
-                name     = EscapeTlmElemName_(tlm['data'][j][1])
-                var_type = tlm['data'][j][2]
-                if comment == "" and name == "":                    # CommentもNameも空白なら打ち切り
+            for j in range(DATA_START_ROW, len(tlm["data"])):
+                comment = tlm["data"][j][0]
+                name = EscapeTlmElemName_(tlm["data"][j][1])
+                var_type = tlm["data"][j][2]
+                if comment == "" and name == "":  # CommentもNameも空白なら打ち切り
                     break
                 if comment != "":
                     continue
@@ -73,7 +82,7 @@ def GenerateTlmBuffer(settings, other_obc_dbs):
                 # name_tree = name.lower().split(".")[2:]     # OBC名.テレメ名.HOGE.FUGA を想定
                 name_tree = name.lower().split(".")
                 name_path = "/".join(name_tree)
-                if (SetStructTree_(tlm_struct_tree, name_path, var_type)):
+                if SetStructTree_(tlm_struct_tree, name_path, var_type):
                     print("Error: Tlm DB Struct Parse Err at " + name, file=sys.stderr)
                     sys.exit(1)
 
@@ -91,19 +100,30 @@ def GenerateTlmBuffer(settings, other_obc_dbs):
         body_h += "\n"
         body_h += "void {_obc_name_upper}_buffer_init(void);\n"
         body_h += "\n"
-        body_h += "DS_ERR_CODE {_obc_name_upper}_buffer_tlm_contents(DS_StreamConfig* p_stream_config, " + driver_type + "* " + driver_name + ");\n"
-
+        body_h += (
+            "DS_ERR_CODE {_obc_name_upper}_buffer_tlm_contents(DS_StreamConfig* p_stream_config, "
+            + driver_type
+            + "* "
+            + driver_name
+            + ");\n"
+        )
 
         body_c += "\n"
         body_c += "void {_obc_name_upper}_buffer_init(void)\n"
         body_c += "{{\n"
         for tlm in tlm_db:
-            tlm_name = tlm['tlm_name']
+            tlm_name = tlm["tlm_name"]
             tlm_name_lower = tlm_name.lower()
             body_c += "  {_obc_name_lower}_buffer_." + tlm_name_lower + ".size = 0;\n"
         body_c += "}}\n"
         body_c += "\n"
-        body_c += "DS_ERR_CODE {_obc_name_upper}_buffer_tlm_contents(DS_StreamConfig* p_stream_config, " + driver_type + "* " + driver_name + ")\n"
+        body_c += (
+            "DS_ERR_CODE {_obc_name_upper}_buffer_tlm_contents(DS_StreamConfig* p_stream_config, "
+            + driver_type
+            + "* "
+            + driver_name
+            + ")\n"
+        )
         body_c += "{{\n"
 
         body_c += "  uint8_t tlm_id  = DS_C2AFMT_get_tlm_id(p_stream_config);\n"
@@ -112,11 +132,17 @@ def GenerateTlmBuffer(settings, other_obc_dbs):
         body_c += "  switch (tlm_id)\n"
         body_c += "  {{\n"
         for tlm in tlm_db:
-            tlm_name = tlm['tlm_name']
+            tlm_name = tlm["tlm_name"]
             tlm_name_upper = tlm_name.upper()
             tlm_name_lower = tlm_name.lower()
             body_c += "  case {_obc_name_upper}_Tlm_CODE_" + tlm_name_upper + ":\n"
-            body_c += "    return {_obc_name_upper}_analyze_tlm_" + tlm_name_lower + "_(p_stream_config, " + driver_name + ");\n"
+            body_c += (
+                "    return {_obc_name_upper}_analyze_tlm_"
+                + tlm_name_lower
+                + "_(p_stream_config, "
+                + driver_name
+                + ");\n"
+            )
         body_c += "  default:\n"
         body_c += "    " + settings["other_obc_data"][i]["code_when_tlm_not_found"] + "\n"
         body_c += "    return DS_ERR_CODE_ERR;\n"
@@ -125,29 +151,37 @@ def GenerateTlmBuffer(settings, other_obc_dbs):
         body_c += "\n"
         for tlm in tlm_db:
             conv_tpye_to_temp = {
-                                    'int8_t'   : 'temp_i8',
-                                    'int16_t'  : 'temp_i16',
-                                    'int32_t'  : 'temp_i32',
-                                    'uint8_t'  : 'temp_u8',
-                                    'uint16_t' : 'temp_u16',
-                                    'uint32_t' : 'temp_u32',
-                                    'float'    : 'temp_f',
-                                    'double'   : 'temp_d',
-                                }
+                "int8_t": "temp_i8",
+                "int16_t": "temp_i16",
+                "int32_t": "temp_i32",
+                "uint8_t": "temp_u8",
+                "uint16_t": "temp_u16",
+                "uint32_t": "temp_u32",
+                "float": "temp_f",
+                "double": "temp_d",
+            }
             conv_tpye_to_size = {
-                                    'int8_t'   : 1,
-                                    'int16_t'  : 2,
-                                    'int32_t'  : 4,
-                                    'uint8_t'  : 1,
-                                    'uint16_t' : 2,
-                                    'uint32_t' : 4,
-                                    'float'    : 4,
-                                    'double'   : 8
-                                }
-            tlm_name = tlm['tlm_name']
+                "int8_t": 1,
+                "int16_t": 2,
+                "int32_t": 4,
+                "uint8_t": 1,
+                "uint16_t": 2,
+                "uint32_t": 4,
+                "float": 4,
+                "double": 8,
+            }
+            tlm_name = tlm["tlm_name"]
             tlm_name_upper = tlm_name.upper()
             tlm_name_lower = tlm_name.lower()
-            body_c += "static DS_ERR_CODE {_obc_name_upper}_analyze_tlm_" + tlm_name_lower + "_(DS_StreamConfig* p_stream_config, " + driver_type + "* " + driver_name + ")\n"
+            body_c += (
+                "static DS_ERR_CODE {_obc_name_upper}_analyze_tlm_"
+                + tlm_name_lower
+                + "_(DS_StreamConfig* p_stream_config, "
+                + driver_type
+                + "* "
+                + driver_name
+                + ")\n"
+            )
             body_c += "{{\n"
             body_c += "  uint32_t tlm_len = DS_ISSLFMT_get_tlm_length(p_stream_config);\n"
             body_c += "  const uint8_t* f = DSSC_get_rx_frame(p_stream_config) + DS_ISSLFMT_COMMON_HEADER_SIZE;\n"
@@ -163,16 +197,22 @@ def GenerateTlmBuffer(settings, other_obc_dbs):
             body_c += "\n"
             body_c += "  // GSへのテレメ中継のためのバッファーへのコピー\n"
             body_c += "  if (contents_len > {_obc_name_upper}_TELEMETRY_BUFFE_SIZE) return DS_ERR_CODE_ERR;\n"
-            body_c += "  memcpy({_obc_name_lower}_buffer_." + tlm_name_lower + ".buffer, contents_pos, (size_t)contents_len);\n"
-            body_c += "  {_obc_name_lower}_buffer_." + tlm_name_lower + ".size = (int)contents_len;\n"
+            body_c += (
+                "  memcpy({_obc_name_lower}_buffer_."
+                + tlm_name_lower
+                + ".buffer, contents_pos, (size_t)contents_len);\n"
+            )
+            body_c += (
+                "  {_obc_name_lower}_buffer_." + tlm_name_lower + ".size = (int)contents_len;\n"
+            )
             body_c += "\n"
             body_c += "  // MOBC内部でテレメデータへアクセスしやすいようにするための構造体へのパース\n"
             last_var_type = ""
-            for j in range(DATA_START_ROW, len(tlm['data'])):
-                comment  = tlm['data'][j][0]
-                name     = EscapeTlmElemName_(tlm['data'][j][1])
-                var_type = tlm['data'][j][2]
-                if comment == "" and name == "":                    # CommentもNameも空白なら打ち切り
+            for j in range(DATA_START_ROW, len(tlm["data"])):
+                comment = tlm["data"][j][0]
+                name = EscapeTlmElemName_(tlm["data"][j][1])
+                var_type = tlm["data"][j][2]
+                if comment == "" and name == "":  # CommentもNameも空白なら打ち切り
                     break
                 if comment != "":
                     continue
@@ -184,26 +224,54 @@ def GenerateTlmBuffer(settings, other_obc_dbs):
                 if last_var_type == "":
                     continue
 
-                oct_pos = int(tlm['data'][j][5])
-                bit_pos = int(tlm['data'][j][6])
-                bit_len = int(tlm['data'][j][7])
-                is_compression = 0              # テレメ圧縮フラグ for ビットフィールドをつかってる奴ら
-                if tlm['data'][j][2] == "" or tlm['data'][j+1][2] == "":
+                oct_pos = int(tlm["data"][j][5])
+                bit_pos = int(tlm["data"][j][6])
+                bit_len = int(tlm["data"][j][7])
+                is_compression = 0  # テレメ圧縮フラグ for ビットフィールドをつかってる奴ら
+                if tlm["data"][j][2] == "" or tlm["data"][j + 1][2] == "":
                     is_compression = 1
-                if (tlm['data'][j+1][0] == "" and tlm['data'][j+1][1] == "" and tlm['data'][j][2] != ""):    # 最終行の除外
+                if (
+                    tlm["data"][j + 1][0] == ""
+                    and tlm["data"][j + 1][1] == ""
+                    and tlm["data"][j][2] != ""
+                ):  # 最終行の除外
                     is_compression = 0
 
                 # name_tree = name.lower().split(".")[2:]     # OBC名.テレメ名.HOGE.FUGA を想定
                 name_tree = name.lower().split(".")
                 name_path = ".".join(name_tree)
                 var_name = driver_name + "->tlm_data." + tlm_name_lower + "." + name_path
-                if (is_compression):
-                    body_c += "  endian_memcpy(&" + conv_tpye_to_temp[var_type] + ", &(f[" + str(oct_pos) + "]), " + str(conv_tpye_to_size[var_type]) + ");\n"
-                    body_c += "  " + conv_tpye_to_temp[var_type] + " >>= " + str(7 - bit_pos) + ";\n"
-                    body_c += "  " + conv_tpye_to_temp[var_type] + " &= " + hex(int("0b" + "1" * bit_len, 2)) + ";\n"
+                if is_compression:
+                    body_c += (
+                        "  endian_memcpy(&"
+                        + conv_tpye_to_temp[var_type]
+                        + ", &(f["
+                        + str(oct_pos)
+                        + "]), "
+                        + str(conv_tpye_to_size[var_type])
+                        + ");\n"
+                    )
+                    body_c += (
+                        "  " + conv_tpye_to_temp[var_type] + " >>= " + str(7 - bit_pos) + ";\n"
+                    )
+                    body_c += (
+                        "  "
+                        + conv_tpye_to_temp[var_type]
+                        + " &= "
+                        + hex(int("0b" + "1" * bit_len, 2))
+                        + ";\n"
+                    )
                     body_c += "  " + var_name + " = " + conv_tpye_to_temp[var_type] + ";\n"
                 else:
-                    body_c += "  endian_memcpy(&(" + var_name + "), &(f[" + str(oct_pos) + "]), " + str(conv_tpye_to_size[var_type]) + ");\n"
+                    body_c += (
+                        "  endian_memcpy(&("
+                        + var_name
+                        + "), &(f["
+                        + str(oct_pos)
+                        + "]), "
+                        + str(conv_tpye_to_size[var_type])
+                        + ");\n"
+                    )
 
             body_c += "  // TODO: ビットフィールドをつかっている系は，様々なパターンがあり得るので，今後，バグが出ないか注視する\n"
             body_c += "\n"
@@ -215,11 +283,23 @@ def GenerateTlmBuffer(settings, other_obc_dbs):
             body_c += "}}\n"
             body_c += "\n"
 
-
-        output_file_path = settings["c2a_root_dir"] + r"src_user/Drivers/" + settings["other_obc_data"][i]["driver_path"];
-        OutputTlmBufferC_(output_file_path + obc_name.lower() + "_telemetry_buffer.c", obc_name, body_c, settings)
-        OutputTlmBufferH_(output_file_path + obc_name.lower() + "_telemetry_buffer.h", obc_name, body_h, settings)
-        OutputTlmDataDefH_(output_file_path + obc_name.lower() + "_telemetry_data_definitions.h", obc_name, tlmdef_body_h, settings)
+        output_file_path = (
+            settings["c2a_root_dir"]
+            + r"src_user/Drivers/"
+            + settings["other_obc_data"][i]["driver_path"]
+        )
+        OutputTlmBufferC_(
+            output_file_path + obc_name.lower() + "_telemetry_buffer.c", obc_name, body_c, settings
+        )
+        OutputTlmBufferH_(
+            output_file_path + obc_name.lower() + "_telemetry_buffer.h", obc_name, body_h, settings
+        )
+        OutputTlmDataDefH_(
+            output_file_path + obc_name.lower() + "_telemetry_data_definitions.h",
+            obc_name,
+            tlmdef_body_h,
+            settings,
+        )
 
 
 def OutputTlmBufferC_(file_path, name, body, settings):
@@ -228,7 +308,7 @@ def OutputTlmBufferC_(file_path, name, body, settings):
     name_capit = name.capitalize()
 
     output = ""
-    output += '''
+    output += """
 #pragma section REPRO
 /**
  * @file
@@ -240,16 +320,24 @@ def OutputTlmBufferC_(file_path, name, body, settings):
 #include "./{_obc_name_lower}_telemetry_buffer.h"
 #include <string.h> // for memcpy
 
-'''[1:]         # 最初の改行を除く
+"""[
+        1:
+    ]  # 最初の改行を除く
 
     output += body
 
-    output += '''
+    output += """
 #pragma section
-'''[1:]         # 最初の改行を除く
+"""[
+        1:
+    ]  # 最初の改行を除く
 
-    with open(file_path, mode='w', encoding=settings['output_file_encoding']) as fh:
-        fh.write(output.format(_obc_name_upper=name_upper, _obc_name_lower=name_lower, _obc_name_capit=name_capit))
+    with open(file_path, mode="w", encoding=settings["output_file_encoding"]) as fh:
+        fh.write(
+            output.format(
+                _obc_name_upper=name_upper, _obc_name_lower=name_lower, _obc_name_capit=name_capit
+            )
+        )
 
 
 def OutputTlmBufferH_(file_path, name, body, settings):
@@ -258,7 +346,7 @@ def OutputTlmBufferH_(file_path, name, body, settings):
     name_capit = name.capitalize()
 
     output = ""
-    output += '''
+    output += """
 /**
  * @file
  * @brief  テレメトリバッファー（テレメ中継）
@@ -269,17 +357,25 @@ def OutputTlmBufferH_(file_path, name, body, settings):
 
 #include "./{_obc_name_lower}.h"
 
-'''[1:]         # 最初の改行を除く
+"""[
+        1:
+    ]  # 最初の改行を除く
 
     output += body
 
-    output += '''
+    output += """
 
 #endif
-'''[1:]         # 最初の改行を除く
+"""[
+        1:
+    ]  # 最初の改行を除く
 
-    with open(file_path, mode='w', encoding=settings['output_file_encoding']) as fh:
-        fh.write(output.format(_obc_name_upper=name_upper, _obc_name_lower=name_lower, _obc_name_capit=name_capit))
+    with open(file_path, mode="w", encoding=settings["output_file_encoding"]) as fh:
+        fh.write(
+            output.format(
+                _obc_name_upper=name_upper, _obc_name_lower=name_lower, _obc_name_capit=name_capit
+            )
+        )
 
 
 def OutputTlmDataDefH_(file_path, name, body, settings):
@@ -288,7 +384,7 @@ def OutputTlmDataDefH_(file_path, name, body, settings):
     name_capit = name.capitalize()
 
     output = ""
-    output += '''
+    output += """
 /**
  * @file
  * @brief  バッファリングされているテレメをパースしてMOBC内でかんたんに利用できるようにするためのテレメデータ構造体定義
@@ -297,21 +393,30 @@ def OutputTlmDataDefH_(file_path, name, body, settings):
 #ifndef {_obc_name_upper}_TELEMETRY_DATA_DEFINITIONS_H_
 #define {_obc_name_upper}_TELEMETRY_DATA_DEFINITIONS_H_
 
-'''[1:]         # 最初の改行を除く
+"""[
+        1:
+    ]  # 最初の改行を除く
 
     output += body
 
-    output += '''
+    output += """
 
 #endif
-'''[1:]         # 最初の改行を除く
+"""[
+        1:
+    ]  # 最初の改行を除く
 
-    with open(file_path, mode='w', encoding=settings['output_file_encoding']) as fh:
-        fh.write(output.format(_obc_name_upper=name_upper, _obc_name_lower=name_lower, _obc_name_capit=name_capit))
+    with open(file_path, mode="w", encoding=settings["output_file_encoding"]) as fh:
+        fh.write(
+            output.format(
+                _obc_name_upper=name_upper, _obc_name_lower=name_lower, _obc_name_capit=name_capit
+            )
+        )
 
 
 def GetStructTree_(dict, path, sep="/", default=None):
     path_list = path.split(sep)
+
     def _(dict, path_list, sep, default):
         if len(path_list) == 0:
             return default
@@ -319,18 +424,20 @@ def GetStructTree_(dict, path, sep="/", default=None):
             return dict.get(path_list[0], default)
         else:
             return _(dict.get(path_list[0], {}), path_list[1:], sep, default)
+
     return _(dict, path_list, sep=sep, default=None)
 
 
 def SetStructTree_(dict, path, val, sep="/"):
     path_list = path.split(sep)
+
     def _(dict, path_list, val, sep):
         if len(path_list) == 0:
-            return 1            # err
+            return 1  # err
         if len(path_list) == 1:
             key = path_list[0]
             if key in dict:
-                return 1        # 上書きエラー
+                return 1  # 上書きエラー
             else:
                 dict[key] = val
                 return 0
@@ -339,6 +446,7 @@ def SetStructTree_(dict, path, val, sep="/"):
             if not key in dict:
                 dict[key] = {}
             return _(dict[key], path_list[1:], val, sep)
+
     return _(dict, path_list, val, sep=sep)
 
 
@@ -349,11 +457,12 @@ def GenerateStructDef_(tree, name):
         output += " " * (indent) + "{{\n"
         for k, v in tree.items():
             if type(v) == dict:
-                output += _(v, k, indent+2)
+                output += _(v, k, indent + 2)
                 continue
-            output += " " * (indent+2) + v + " " + k + ";\n"
+            output += " " * (indent + 2) + v + " " + k + ";\n"
         output += " " * (indent) + "}} " + name + ";\n"
         return output
+
     return _(tree, name, 2)
 
 
