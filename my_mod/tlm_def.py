39737242d4b31cb814a214bd76a5ecf4b2286c44
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-02-01 04:02:22 +0900

    update for include header to tlm contents

--- my_mod/tlm_def.py
+++ my_mod/tlm_def.py
@@ -15,14 +15,10 @@ def GenerateTlmDef(settings, tlm_db, other_obc_dbs):
     body_c = ""
     body_h = ""
 
-    # "static int OBC_(unsigned char* contents, int max_len);"
+    # "static int OBC_(uint8_t* packet, int max_len);"
     # "  OBC_ID = 0x00,"
     for tlm in tlm_db:
-        body_c += (
-            "static int Tlm_"
-            + tlm["tlm_name"].upper()
-            + "_(unsigned char* contents, int max_len);\n"
-        )
+        body_c += "static int Tlm_" + tlm["tlm_name"].upper() + "_(uint8_t* packet, int max_len);\n"
         body_h += "  Tlm_CODE_" + tlm["tlm_name"].upper() + " = " + tlm["tlm_id"] + ",\n"
 
     if settings["is_main_obc"]:
@@ -66,7 +62,7 @@ def GenerateTlmDef(settings, tlm_db, other_obc_dbs):
             if pos == "":
                 continue
 
-            pos = int(pos) - settings["header_len"]
+            pos = int(pos)
             code = code.replace("@@", ",")
             func_code += "  "
             if var_type == "int8_t":
@@ -96,14 +92,10 @@ def GenerateTlmDef(settings, tlm_db, other_obc_dbs):
             else:
                 print("Error: Tlm DB Err at " + tlm["tlm_name"].upper(), file=sys.stderr)
                 sys.exit(1)
-            func_code += "(&contents[" + str(pos) + "], " + code + ");\n"
+            func_code += "(&packet[" + str(pos) + "], " + code + ");\n"
 
         body_c += "\n"
-        body_c += (
-            "static int Tlm_"
-            + tlm["tlm_name"].upper()
-            + "_(unsigned char* contents, int max_len)\n"
-        )
+        body_c += "static int Tlm_" + tlm["tlm_name"].upper() + "_(uint8_t* packet, int max_len)\n"
         body_c += "{\n"
         for local_var in tlm["local_vars"]:
             body_c += "  " + local_var + "\n"
@@ -141,9 +133,7 @@ def GetTlmDefCOfOtherObcFunDef_(settings, tlm_db, other_obc_dbs):
         temp_c += "// {_obc_name_upper} TLM\n"
         for tlm in oter_obc_tlm_db:
             temp_c += (
-                "static int Tlm_"
-                + tlm["tlm_name"].upper()
-                + "_(unsigned char* contents, int max_len);\n"
+                "static int Tlm_" + tlm["tlm_name"].upper() + "_(uint8_t* packet, int max_len);\n"
             )
 
         body_c += temp_c.format(
@@ -202,16 +192,14 @@ def GetTlmDefCOfOtherObcFunBody_(settings, tlm_db, other_obc_dbs):
             tlm_name_upper = tlm_name.upper()
             # tlm_name_lower = tlm_name.lower()
             temp_c += "\n"
-            temp_c += (
-                "static int Tlm_" + tlm_name_upper + "_(unsigned char* contents, int max_len)\n"
-            )
+            temp_c += "static int Tlm_" + tlm_name_upper + "_(uint8_t* packet, int max_len)\n"
             temp_c += "{{\n"
             temp_c += (
-                "  return AOBC_pick_up_tlm_buffer("
+                "  return {_obc_name_upper}_pick_up_tlm_buffer("
                 + driver_name
                 + ", {_obc_name_upper}_Tlm_CODE_"
                 + tlm_name_upper
-                + ", contents, max_len);\n"
+                + ", packet, max_len);\n"
             )
             temp_c += "}}\n"
 
