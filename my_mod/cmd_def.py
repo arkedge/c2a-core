433b21318819927b1881351f66aad6eaa8c40c74
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2023-04-04 03:16:26 +0900

    apply black

--- my_mod/cmd_def.py
+++ my_mod/cmd_def.py
@@ -157,7 +157,9 @@ def GenerateOtherObcCmdDef(settings, other_obc_dbs):
             if comment != "":  # Comment
                 continue
             # print(name)
-            _, cmd_code = GetCmdNameAndCmdCode_(name, settings["other_obc_data"][i]["is_cmd_prefix_in_db"])
+            _, cmd_code = GetCmdNameAndCmdCode_(
+                name, settings["other_obc_data"][i]["is_cmd_prefix_in_db"]
+            )
             cmd_code = name_upper + cmd_code
             body_h += "  " + cmd_code + " = " + cmd_id + ",\n"
         # print(body_h)
