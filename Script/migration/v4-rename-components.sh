a3afa0b5772b9a0a422d6e8118718a89502bf500
sksat <sksat@arkedgespace.com>
2023-07-21 19:45:00 +0900

    more migration(IfWrapper -> HAL)

--- Script/migration/v4-rename-components.sh
+++ Script/migration/v4-rename-components.sh
@@ -12,4 +12,12 @@ git mv src/src_user/IfWrapper src/src_user/hal
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_user/IfWrapper/Common#src_user/hal/common#g"
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_user/IfWrapper/Sils#src_user/hal/sils#g"
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_user/IfWrapper/SilsMockup#src_user/hal/sils_mockup#g"
+
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IfWrapper#hal#g"
+find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF wrapper#HAL#g"
+find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF_wrapper#HAL#g"
+find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF Wrapper#HAL#g"
+find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF_WRAPPER#HAL#g"
+
+find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#C2A_IF#C2A_HAL#g"
+find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF_IMPL#HAL_IMPL#g"
