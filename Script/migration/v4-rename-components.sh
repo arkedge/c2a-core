ddca69475d9be045e15c7191dfac649780fa33bb
sksat <sksat@arkedgespace.com>
2023-07-21 19:57:36 +0900

    fix migration(IfWrapper -> HAL)

--- Script/migration/v4-rename-components.sh
+++ Script/migration/v4-rename-components.sh
@@ -5,13 +5,16 @@
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_core/IfWrapper/Common#src_core/hal/common#g"
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_core/IfWrapper#src_core/hal#g"
 ## c2a user code
+mkdir -p src/src_user/hal
 mv src/src_user/IfWrapper/Common src/src_user/hal/common
 mv src/src_user/IfWrapper/Sils src/src_user/hal/sils
 mv src/src_user/IfWrapper/SilsMockup src/src_user/hal/sils_mockup
-mv src/src_user/IfWrapper src/src_user/hal
+mv src/src_user/IfWrapper/* src/src_user/hal/
+
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_user/IfWrapper/Common#src_user/hal/common#g"
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_user/IfWrapper/Sils#src_user/hal/sils#g"
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_user/IfWrapper/SilsMockup#src_user/hal/sils_mockup#g"
+
+find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#SilsMockup#sils_mockup#g"
+find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#Sils#sils#g"
 
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IfWrapper#hal#g"
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF wrapper#HAL#g"
