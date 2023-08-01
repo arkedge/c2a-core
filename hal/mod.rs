f1b2ccaa0f292ef319d83cfa170ee452cd930023
sksat <sksat@arkedgespace.com>
2023-08-01 17:26:31 +0900

    bindgen c2a_core_main & hal

---
+++ hal/mod.rs
@@ -0,0 +1,24 @@
+pub mod aa {
+    include!(concat!(env!("OUT_DIR"), "/aa.rs"));
+}
+pub mod ccsds {
+    include!(concat!(env!("OUT_DIR"), "/ccsds.rs"));
+}
+pub mod i2c {
+    include!(concat!(env!("OUT_DIR"), "/i2c.rs"));
+}
+pub mod pa {
+    include!(concat!(env!("OUT_DIR"), "/pa.rs"));
+}
+pub mod spi {
+    include!(concat!(env!("OUT_DIR"), "/spi.rs"));
+}
+pub mod spwire {
+    include!(concat!(env!("OUT_DIR"), "/spwire.rs"));
+}
+pub mod uart {
+    include!(concat!(env!("OUT_DIR"), "/uart.rs"));
+}
+pub mod wdt {
+    include!(concat!(env!("OUT_DIR"), "/wdt.rs"));
+}
