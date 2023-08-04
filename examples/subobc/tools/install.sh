00ee5f2df830bfc45f5fb6200784c470533d5fcc
sksat <sksat@arkedgespace.com>
2023-08-05 03:33:23 +0900

    change C2A Boom tools to debug build

--- examples/subobc/tools/install.sh
+++ examples/subobc/tools/install.sh
@@ -2,10 +2,10 @@
 
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
-cargo install --root . tmtc-c2a --git https://github.com/arkedge/gaia.git --tag v0.5.0
+cargo install --debug --root . tmtc-c2a --git https://github.com/arkedge/gaia.git --tag v0.5.0
 
-cargo install --root . tlmcmddb-cli    --version 0.2.0
-cargo install --root . kble            --version 0.2.0
-cargo install --root . kble-c2a        --version 0.2.0
-cargo install --root . kble-eb90       --version 0.2.0
-cargo install --root . jrsonnet        --version 0.5.0-pre9
+cargo install --debug --root . tlmcmddb-cli    --version 0.2.0
+cargo install --debug --root . kble            --version 0.2.0
+cargo install --debug --root . kble-c2a        --version 0.2.0
+cargo install --debug --root . kble-eb90       --version 0.2.0
+cargo install --debug --root . jrsonnet        --version 0.5.0-pre9
