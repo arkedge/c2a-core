9b52d53274e57568373a3bd4751463712a76a698
sksat <sksat@arkedgespace.com>
2023-08-05 03:54:00 +0900

    add wings utils comment

--- examples/mobc/src/src_user/Test/utils/wings_utils.py
+++ examples/mobc/src/src_user/Test/utils/wings_utils.py
@@ -4,6 +4,7 @@ import json
 from c2a_pytest_gaia import wings_compat
 
 
+# WINGS API 互換の c2a-pytest-gaia を使用
 def get_wings_operation():
     tlmcmddb = json.load(open("../../../tlmcmddb.json"))
     return wings_compat.Operation(tlmcmddb)
