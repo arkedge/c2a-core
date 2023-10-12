54d51f1843cc46d7e26ef9dd86039362e06da962
sksat <sksat@arkedgespace.com>
2023-10-13 02:49:28 +0900

    update v4 application migration to rename src/src_user/applications/UserDefined/*

--- script/migration/v4-rename-applications.sh
+++ script/migration/v4-rename-applications.sh
@@ -11,11 +11,21 @@ find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#s
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#\${C2A_CORE_DIR}/Applications#\${C2A_CORE_DIR}/applications#g"
 
 # C2A user code
+user_defined_org=("Aocs" "Cdh" "Com" "Etc" "Mission" "Power" "Prop" "Thermal")
+user_defined_new=("aocs" "cdh" "com" "etc" "mission" "power" "prop" "thermal")
+
 mv src/src_user/Applications src/src_user/applications
 
 mv src/src_user/applications/DriverInstances src/src_user/applications/driver_instances
 mv src/src_user/applications/Middleware src/src_user/applications/middleware
 mv src/src_user/applications/UserDefined src/src_user/applications/user_defined
+
+echo "rename dir: applications/user_defined/"
+for ((i=0; i<${#user_defined_org[@]}; i++)); do
+  echo "  ${user_defined_org[i]} -> ${user_defined_new[i]}"
+
+  mv "src/src_user/applications/user_defined/${user_defined_org[i]}" "src/src_user/applications/user_defined/${user_defined_new[i]}"
+done
 
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#Applications/DriverInstances#applications/driver_instances#g"
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#Applications\\\DriverInstances#applications\\\driver_instances#g"
@@ -32,6 +42,15 @@ find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#\
 # CMake
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#DriverInstances/#driver_instances/#g"
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#Middleware/#middleware/#g"
+
+echo "rename ref: UserDefined"
+for ((i=0; i<${#user_defined_org[@]}; i++)); do
+  echo "  ${user_defined_org[i]} -> ${user_defined_new[i]}"
+
+  find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#UserDefined/${drv_org[i]}#user_defined/${drv_new[i]}#g"
+  find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#UserDefined\\\${drv_org}#user_defined\\\${drv_new}#g"
+done
+
 find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#UserDefined/#user_defined/#g"
 
 # Settings
