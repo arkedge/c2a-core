12944f2593355780aaa21d192d4efaa9d9b2f19a
sksat <sksat@arkedgespace.com>
2023-10-13 17:36:37 +0900

    fix ComponentDriverSuper settings include

--- component_driver/driver_super.h
+++ component_driver/driver_super.h
@@ -34,7 +34,7 @@
                                                        IF_RX から受信できる最大数を規定する
                                                        OBC の物理的な信号ラインのバッファサイズ以上とするともっともパフォーマンスが出る */
 
-#include <src_user/Settings/ComponentDriverSuper/driver_super_params.h>
+#include <src_user/Settings/component_driver_super/driver_super_params.h>
 
 typedef struct ComponentDriverSuper ComponentDriverSuper;
 typedef struct CDS_StreamConfig CDS_StreamConfig;
