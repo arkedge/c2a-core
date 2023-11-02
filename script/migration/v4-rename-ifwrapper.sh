43d39fccd687a529807705a2e7a98b0d4dd099cc
sksat <sksat@arkedgespace.com>
2023-11-02 17:03:09 +0900

    refactor & fix ifwrapper rename migration script

--- script/migration/v4-rename-ifwrapper.sh
+++ script/migration/v4-rename-ifwrapper.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
-# IfWrapper -> hal
+function find_all() {
+  find . -name "*" -not \( -path "*/.git/*" -o -path "*/src_core/*" -o -path "*.xlsm" \) -type f -print0
+}
+
+echo "IfWrapper -> hal"
+echo ""
+
 ## c2a-core ref
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_core/IfWrapper/Common/i2c_common#src_core/hal/i2c_common#g"
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_core/IfWrapper#src_core/hal#g"
+echo "rename c2a-core ref: src_core/IfWrapper -> src_core/hal"
+find_all | xargs -0 sed -i -e "s#src_core/IfWrapper/Common/i2c_common#src_core/hal/i2c_common#g"
+find_all | xargs -0 sed -i -e "s#src_core/IfWrapper#src_core/hal#g"
+
 ## c2a user code
-mkdir -p src/src_user/hal
-mv src/src_user/IfWrapper/Common src/src_user/hal/common
-mv src/src_user/IfWrapper/Sils src/src_user/hal/sils
-mv src/src_user/IfWrapper/SilsMockup src/src_user/hal/sils_mockup
-mv src/src_user/IfWrapper/* src/src_user/hal/
+echo "rename src_user dir: src_user/IfWrapper -> src_user/hal"
+mv src/src_user/IfWrapper src/src_user/hal
+mv src/src_user/hal/Common src/src_user/hal/common
+mv src/src_user/hal/Sils src/src_user/hal/sils
+mv src/src_user/hal/SilsMockup src/src_user/hal/sils_mockup
 
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#src_user/IfWrapper/Common#src_user/hal/common#g"
+### ref
+echo "rename src_user ref: IfWrapper/*"
+find_all | xargs -0 sed -i -e "s#IfWrapper/Common#hal/common#g"
+find_all | xargs -0 sed -i -e "s#IfWrapper/Sils#IfWrapper/sils#g"
 
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#SilsMockup#sils_mockup#g"
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#Sils#sils#g"
+find_all | xargs -0 sed -i -e "s#SilsMockup#sils_mockup#g"
 
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IfWrapper#hal#g"
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF wrapper#HAL#g"
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF_wrapper#HAL#g"
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF Wrapper#HAL#g"
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF_WRAPPER#HAL#g"
+#### IfWrapper internal ref
+echo "rename src_user ref in src/src_user/hal: IfWrapper/*"
+find src/src_user/hal -name "*" -type f -print0 | xargs -0 sed -i -e "s#Common#common#g"
 
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#C2A_IF#C2A_HAL#g"
-find . -name "*" -not -path "*/.git/*" -type f -print0 | xargs -0 sed -i -e "s#IF_IMPL#HAL_IMPL#g"
+#### main ref
+echo "rename IfWrapper -> hal"
+find_all | xargs -0 sed -i -e "s#IfWrapper#hal#g"
+find_all | xargs -0 sed -i -e "s#IF wrapper#HAL#g"
+find_all | xargs -0 sed -i -e "s#IF_wrapper#HAL#g"
+find_all | xargs -0 sed -i -e "s#IF Wrapper#HAL#g"
+find_all | xargs -0 sed -i -e "s#IF_WRAPPER#HAL#g"
+
+find_all | xargs -0 sed -i -e "s#C2A_IF#C2A_HAL#g"
+find_all | xargs -0 sed -i -e "s#IF_IMPL#HAL_IMPL#g"
