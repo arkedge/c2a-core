c66faff4f7381e6d22b2f3cb4f6d522507956b3b
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2023-11-09 00:23:42 +0900

    fix small

--- examples/mobc/src/src_user/tlm_cmd/ccsds/multiplexing_protocol_data_unit.h
+++ examples/mobc/src/src_user/tlm_cmd/ccsds/multiplexing_protocol_data_unit.h
@@ -22,9 +22,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define M_PDU_HEADER_SIZE (2u)
-#define M_PDU_DATA_SIZE (432u)
-#define M_PDU_LEN (M_PDU_HEADER_SIZE + M_PDU_DATA_SIZE)
+#define M_PDU_HEADER_SIZE   (2u)
+#define M_PDU_DATA_SIZE     (432u)
+#define M_PDU_LEN           (M_PDU_HEADER_SIZE + M_PDU_DATA_SIZE)
 
 typedef struct
 {
@@ -34,8 +34,8 @@ typedef struct
 
 typedef enum
 {
-  M_PDU_PTR_NO_HDR = 0x07ff, // 11111111111b
-  M_PDU_PTR_IDLE = 0x7fe // 11111111110b
+  M_PDU_PTR_NO_HDR = 0x07ff,  // 11111111111b
+  M_PDU_PTR_IDLE = 0x7fe      // 11111111110b
 } M_PDU_PTR;
 
 void M_PDU_generate_byte_stream(const M_PDU* m_pdu, uint8_t byte_stream[M_PDU_LEN]);
@@ -46,7 +46,8 @@ uint16_t M_PDU_get_1st_hdr_ptr(const M_PDU* m_pdu);
 
 void M_PDU_set_1st_hdr_ptr(M_PDU* m_pdu, uint16_t ptr);
 
-const uint8_t* M_PDU_get_data_head(const M_PDU* m_pdu);
+// FIXME: 存在しなかったためコメントアウトした
+// const uint8_t* M_PDU_get_data_head(const M_PDU* m_pdu);
 
 void M_PDU_set_data(M_PDU* m_pdu, const uint8_t* data, size_t offset, size_t len);
 
