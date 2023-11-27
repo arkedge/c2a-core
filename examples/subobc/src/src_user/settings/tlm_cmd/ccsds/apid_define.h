8f557355630c9708b4c1acec9f06b4b65271d87a
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2023-11-27 17:54:24 +0900

    fix comment

--- examples/subobc/src/src_user/settings/tlm_cmd/ccsds/apid_define.h
+++ examples/subobc/src/src_user/settings/tlm_cmd/ccsds/apid_define.h
@@ -12,18 +12,7 @@
  * @enum   APID
  * @brief  Application Process ID
  * @note   11bit
- * @note   命名規則:
- *           Tlm: APID_TLM_{送信元}_TO_{受信先}_VIA_{経由地}
- *           Cmd: APID_CMD_{送信元}_TO_{受信先}_VIA_{経由地}
- *           ただし，Tlm の TO_SGS，Cmd の SGS は省略できる
- *           例:
- *             APID_CMD_TO_MOBC (SGS を省略)
- *             APID_TLM_AOBC (TO_SGS を省略．パスが単一しかありえないので， VIA_MOBCも省略)
- *             APID_TLM_CAMERA_TO_XGS_VIA_MIF (全部記載)
- *           注意:
- *             同一コンポからアプリケーション等を区別して APID を発行したい場合 (eg; HK テレメ，ミッションデータ，Sバンドデータ，X バンドデータ，など) は {送信元} にそれを識別する命名ができる
- *             APID_TLM_AOBC, APID_TLM_AOBC_STT_IMG, など
- *           詳細: https://github.com/arkedge/c2a-core/issues/186#issuecomment-1798685321
+ * @note   命名規則は tlm_cmd/common_tlm_cmd_packet.h を参照
  */
 typedef enum
 {
