a43d57a84cfa33ef794a57f48e48d2c59c9dd52c
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2023-12-29 02:54:38 +0900

    Update test_ccp_dump.py

--- examples/mobc/src/src_user/test/src_core/applications/test_ccp_dump.py
+++ examples/mobc/src/src_user/test/src_core/applications/test_ccp_dump.py
@@ -84,23 +84,15 @@ def test_ccp_dump_cdis():
     tlm_CCP_DUMP = get_ccp_dump_tlm()
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.TARGET"] == "CDIS"
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.STATUS"] == "OK"
-    assert tlm_CCP_DUMP["CCP_DUMP.SH.TI"] - tlm_CCP_DUMP["CCP_DUMP.DUMP.DUMP_TIME.TOTAL_CYCLE"] > 0
-    assert (
-        tlm_CCP_DUMP["CCP_DUMP.SH.TI"] - tlm_CCP_DUMP["CCP_DUMP.DUMP.DUMP_TIME.TOTAL_CYCLE"] < 100
-    )
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.VER"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.TYPE"] == 1
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.SH_FLAG"] == 1
+    check_common_ccp(tlm_CCP_DUMP)
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.PACKET_LEN"] == 11
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.VER"] == 1
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_TYPE"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_ID"] == tlc0.id
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.DEST_TYPE"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.EXEC_TYPE"] == 1
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_ID"], 16) == tlc0.id
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.DEST_TYPE"], 16) == 0
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.EXEC_TYPE"], 16) == 1
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.TI"] == tlc0.ti
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM0"] == tlc0.params[0]
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM1"] == tlc0.params[1]
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM2"] == tlc0.params[2]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM0"], 16) == tlc0.params[0]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM1"], 16) == tlc0.params[1]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM2"], 16) == tlc0.params[2]
 
     assert "SUC" == wings.util.send_rt_cmd_and_confirm(
         ope, c2a_enum.Cmd_CODE_CCP_DUMP_CDIS, (CDIS_IDX_OF_TL_GS, 1), c2a_enum.Tlm_CODE_HK
@@ -108,23 +100,15 @@ def test_ccp_dump_cdis():
     tlm_CCP_DUMP = get_ccp_dump_tlm()
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.TARGET"] == "CDIS"
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.STATUS"] == "OK"
-    assert tlm_CCP_DUMP["CCP_DUMP.SH.TI"] - tlm_CCP_DUMP["CCP_DUMP.DUMP.DUMP_TIME.TOTAL_CYCLE"] > 0
-    assert (
-        tlm_CCP_DUMP["CCP_DUMP.SH.TI"] - tlm_CCP_DUMP["CCP_DUMP.DUMP.DUMP_TIME.TOTAL_CYCLE"] < 100
-    )
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.VER"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.TYPE"] == 1
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.SH_FLAG"] == 1
+    acheck_common_ccp(tlm_CCP_DUMP)
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.PACKET_LEN"] == 11
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.VER"] == 1
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_TYPE"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_ID"] == tlc1.id
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.DEST_TYPE"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.EXEC_TYPE"] == 1
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_ID"], 16) == tlc1.id
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.DEST_TYPE"], 16) == 0
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.EXEC_TYPE"], 16) == 1
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.TI"] == tlc1.ti
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM0"] == tlc1.params[0]
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM1"] == tlc1.params[1]
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM2"] == tlc1.params[2]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM0"], 16) == tlc1.params[0]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM1"], 16) == tlc1.params[1]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM2"], 16) == tlc1.params[2]
 
     # 空の TL0
     clear_tl0()
@@ -147,8 +131,8 @@ def test_ccp_dump_bct():
     bc0 = init_cmd_class(10000, c2a_enum.Cmd_CODE_TG_FORWARD_AS_RT_TLM, [0x12, 0x34, 0x56])
     bc1 = init_cmd_class(20000, c2a_enum.Cmd_CODE_TG_FORWARD_AS_RT_TLM, [0xAB, 0xCD, 0xEF])
 
-    wings.util.send_bl_cmd(ope, bc0.ti, bc0.id, (0x1234, 0x56))
-    wings.util.send_bl_cmd(ope, bc0.ti, bc0.id, (0xABCD, 0xEF))
+    ope.send_bl_cmd(bc0.ti, bc0.id, (0x1234, 0x56))
+    ope.send_bl_cmd(bc0.ti, bc0.id, (0xABCD, 0xEF))
 
     # BCT 確認
     assert "SUC" == wings.util.send_rt_cmd_and_confirm(
@@ -157,23 +141,15 @@ def test_ccp_dump_bct():
     tlm_CCP_DUMP = get_ccp_dump_tlm()
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.TARGET"] == "BCT"
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.STATUS"] == "OK"
-    assert tlm_CCP_DUMP["CCP_DUMP.SH.TI"] - tlm_CCP_DUMP["CCP_DUMP.DUMP.DUMP_TIME.TOTAL_CYCLE"] > 0
-    assert (
-        tlm_CCP_DUMP["CCP_DUMP.SH.TI"] - tlm_CCP_DUMP["CCP_DUMP.DUMP.DUMP_TIME.TOTAL_CYCLE"] < 100
-    )
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.VER"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.TYPE"] == 1
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.SH_FLAG"] == 1
+    check_common_ccp(tlm_CCP_DUMP)
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.PACKET_LEN"] == 11
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.VER"] == 1
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_TYPE"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_ID"] == bc0.id
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.DEST_TYPE"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.EXEC_TYPE"] == 1
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_ID"], 16) == bc0.id
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.DEST_TYPE"], 16) == 0
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.EXEC_TYPE"], 16) == 2
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.TI"] == bc0.ti
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM0"] == bc0.params[0]
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM1"] == bc0.params[1]
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM2"] == bc0.params[2]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM0"], 16) == bc0.params[0]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM1"], 16) == bc0.params[1]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM2"], 16) == bc0.params[2]
 
     assert "SUC" == wings.util.send_rt_cmd_and_confirm(
         ope, c2a_enum.Cmd_CODE_CCP_DUMP_BCT, (c2a_enum.BC_TEST_USE_PYTEST, 1), c2a_enum.Tlm_CODE_HK
@@ -181,23 +157,15 @@ def test_ccp_dump_bct():
     tlm_CCP_DUMP = get_ccp_dump_tlm()
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.TARGET"] == "BCT"
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.STATUS"] == "OK"
-    assert tlm_CCP_DUMP["CCP_DUMP.SH.TI"] - tlm_CCP_DUMP["CCP_DUMP.DUMP.DUMP_TIME.TOTAL_CYCLE"] > 0
-    assert (
-        tlm_CCP_DUMP["CCP_DUMP.SH.TI"] - tlm_CCP_DUMP["CCP_DUMP.DUMP.DUMP_TIME.TOTAL_CYCLE"] < 100
-    )
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.VER"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.TYPE"] == 1
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.SH_FLAG"] == 1
+    check_common_ccp(tlm_CCP_DUMP)
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.PACKET_LEN"] == 11
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.VER"] == 1
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_TYPE"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_ID"] == bc1.id
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.DEST_TYPE"] == 0
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.EXEC_TYPE"] == 1
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_ID"], 16) == bc1.id
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.DEST_TYPE"], 16) == 0
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.EXEC_TYPE"], 16) == 2
     assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.TI"] == bc1.ti
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM0"] == bc1.params[0]
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM1"] == bc1.params[1]
-    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM2"] == bc1.params[2]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM0"], 16) == bc1.params[0]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM1"], 16) == bc1.params[1]
+    assert int(tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PATAM2"], 16) == bc1.params[2]
 
 
 def get_ccp_dump_tlm():
@@ -221,6 +189,18 @@ def clear_bct(id):
     assert "SUC" == wings.util.send_rt_cmd_and_confirm(
         ope, c2a_enum.Cmd_CODE_BCT_CLEAR_BLOCK, (id,), c2a_enum.Tlm_CODE_HK
     )
+
+
+def check_common_ccp(tlm_CCP_DUMP):
+    assert tlm_CCP_DUMP["CCP_DUMP.SH.TI"] - tlm_CCP_DUMP["CCP_DUMP.DUMP.DUMP_TIME.TOTAL_CYCLE"] > 0
+    assert (
+        tlm_CCP_DUMP["CCP_DUMP.SH.TI"] - tlm_CCP_DUMP["CCP_DUMP.DUMP.DUMP_TIME.TOTAL_CYCLE"] < 100
+    )
+    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.VER"] == 0
+    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.TYPE"] == 1
+    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.PH.SH_FLAG"] == 1
+    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.VER"] == 1
+    assert tlm_CCP_DUMP["CCP_DUMP.DUMP.CCP.SH.CMD_TYPE"] == 0
 
 
 def init_cmd_class(ti, id, params):
