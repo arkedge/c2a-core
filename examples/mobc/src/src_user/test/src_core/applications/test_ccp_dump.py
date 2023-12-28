ca25324b450fa82ec5aebdf3b752688ea416f6ca
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2023-12-29 02:30:33 +0900

    Update test_ccp_dump.py

--- examples/mobc/src/src_user/test/src_core/applications/test_ccp_dump.py
+++ examples/mobc/src/src_user/test/src_core/applications/test_ccp_dump.py
@@ -16,8 +16,14 @@ c2a_enum = c2a_enum_utils.get_c2a_enum()
 ope = wings_utils.get_wings_operation()
 
 # コードと整合をとる
-CDIS_MGR_MAX_NUM_OF_CDIS = 7
+CDIS_MGR_MAX_NUM_OF_CDIS = 8
 BCT_MAX_BLOCKS = 383
+CDIS_IDX_OF_TL_GS = 3
+
+class tlc:
+    ti: int
+    id: int
+    params: list
 
 
 @pytest.mark.real
@@ -45,11 +51,100 @@ def test_ccp_dump_cmd_assertion():
     assert tlm_CCP_DUMP["DUMP.STATUS"] == "NOT_FOUND"
 
 
+@pytest.mark.real
+@pytest.mark.sils
+def test_ccp_dump_cdis():
+    # 空の TL0
+    clear_tl0()
+    assert "PRM" == wings.util.send_rt_cmd_and_confirm(
+        ope, c2a_enum.Cmd_CODE_CCP_DUMP_CDIS, (CDIS_IDX_OF_TL_GS, 0), c2a_enum.Tlm_CODE_HK
+    )
+    tlm_CCP_DUMP = get_ccp_dump_tlm()
+    assert tlm_CCP_DUMP["DUMP.TARGET"] == "CDIS"
+    assert tlm_CCP_DUMP["DUMP.STATUS"] == "NOT_FOUND"
+
+    # TL 登録
+    now_ti = tlm_CCP_DUMP["SH.TI"]
+
+    tlc0 = init_tlc_class(now_ti + 10000, c2a_enum.Cmd_CODE_TG_FORWARD_RT_TLM, [0x12, 0x34, 0x56])
+    tlc1 = init_tlc_class(now_ti + 20000, c2a_enum.Cmd_CODE_TG_FORWARD_RT_TLM, [0xab, 0xcd, 0xef])
+
+
+    wings.util.send_tl_cmd(ope, tlc0.ti, tlc0.id, (0x1234, 0x56))
+    wings.util.send_tl_cmd(ope, tlc0.ti, tlc0.id, (0xabcd, 0xef))
+
+    # TL 確認
+    assert "SUC" == wings.util.send_rt_cmd_and_confirm(
+        ope, c2a_enum.Cmd_CODE_CCP_DUMP_CDIS, (CDIS_IDX_OF_TL_GS, 0), c2a_enum.Tlm_CODE_HK
+    )
+    tlm_CCP_DUMP = get_ccp_dump_tlm()
+    assert tlm_CCP_DUMP["DUMP.TARGET"] == "CDIS"
+    assert tlm_CCP_DUMP["DUMP.STATUS"] == "OK"
+    assert tlm_CCP_DUMP["SH.TI"] - tlm_CCP_DUMP["DUMP.DUMP_TIME.TOTAL_CYCLE"] > 0
+    assert tlm_CCP_DUMP["SH.TI"] - tlm_CCP_DUMP["DUMP.DUMP_TIME.TOTAL_CYCLE"] < 100
+    assert tlm_CCP_DUMP["DUMP.CCP.PH.VER"] == 0
+    assert tlm_CCP_DUMP["DUMP.CCP.PH.TYPE"] == 1
+    assert tlm_CCP_DUMP["DUMP.CCP.PH.SH_FLAG"] == 1
+    assert tlm_CCP_DUMP["DUMP.CCP.PH.PACKET_LEN"] == 11
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.VER"] == 1
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.CMD_TYPE"] == 0
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.CMD_ID"] == tlc0.id
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.DEST_TYPE"] == 0
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.EXEC_TYPE"] == 1
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.TI"] == tlc0.ti
+    assert tlm_CCP_DUMP["DUMP.CCP.PATAM0"] == tlc0.params[0]
+    assert tlm_CCP_DUMP["DUMP.CCP.PATAM1"] == tlc0.params[1]
+    assert tlm_CCP_DUMP["DUMP.CCP.PATAM2"] == tlc0.params[2]
+
+    assert "SUC" == wings.util.send_rt_cmd_and_confirm(
+        ope, c2a_enum.Cmd_CODE_CCP_DUMP_CDIS, (CDIS_IDX_OF_TL_GS, 1), c2a_enum.Tlm_CODE_HK
+    )
+    tlm_CCP_DUMP = get_ccp_dump_tlm()
+    assert tlm_CCP_DUMP["DUMP.TARGET"] == "CDIS"
+    assert tlm_CCP_DUMP["DUMP.STATUS"] == "OK"
+    assert tlm_CCP_DUMP["SH.TI"] - tlm_CCP_DUMP["DUMP.DUMP_TIME.TOTAL_CYCLE"] > 0
+    assert tlm_CCP_DUMP["SH.TI"] - tlm_CCP_DUMP["DUMP.DUMP_TIME.TOTAL_CYCLE"] < 100
+    assert tlm_CCP_DUMP["DUMP.CCP.PH.VER"] == 0
+    assert tlm_CCP_DUMP["DUMP.CCP.PH.TYPE"] == 1
+    assert tlm_CCP_DUMP["DUMP.CCP.PH.SH_FLAG"] == 1
+    assert tlm_CCP_DUMP["DUMP.CCP.PH.PACKET_LEN"] == 11
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.VER"] == 1
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.CMD_TYPE"] == 0
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.CMD_ID"] == tlc1.id
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.DEST_TYPE"] == 0
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.EXEC_TYPE"] == 1
+    assert tlm_CCP_DUMP["DUMP.CCP.SH.TI"] == tlc1.ti
+    assert tlm_CCP_DUMP["DUMP.CCP.PATAM0"] == tlc1.params[0]
+    assert tlm_CCP_DUMP["DUMP.CCP.PATAM1"] == tlc1.params[1]
+    assert tlm_CCP_DUMP["DUMP.CCP.PATAM2"] == tlc1.params[2]
+
+
 def get_ccp_dump_tlm():
     tlm = wings.util.generate_and_receive_tlm(
         ope, c2a_enum.Cmd_CODE_TG_GENERATE_RT_TLM, c2a_enum.Tlm_CODE_CCP_DUMP
     )
     return tlm
+
+
+# def get_latest_hk_tlm():
+#     tlm, rec_time = ope.get_latest_tlm(c2a_enum.Tlm_CODE_HK)
+#     return tlm
+
+
+def clear_tl0():
+    # TL0 をクリア
+    assert "SUC" == wings.util.send_rt_cmd_and_confirm(
+        ope,
+        c2a_enum.Cmd_CODE_TLCD_CLEAR_ALL_TIMELINE,
+        (c2a_enum.TLCD_ID_FROM_GS,),
+        c2a_enum.Tlm_CODE_HK,
+    )
+
+
+def init_tlc_class(ti, id, params):
+    tlc.ti = ti
+    tlc.id = id
+    tlc.params = params
 
 
 if __name__ == "__main__":
