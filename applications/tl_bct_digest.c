bb2ed0dd7e38cfecb6e0ca0c5caeb703fc3171fc
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2023-12-31 16:38:43 +0900

    add cmd

--- applications/tl_bct_digest.c
+++ applications/tl_bct_digest.c
@@ -14,29 +14,101 @@
  * @param  void
  * @return RESULT
  */
-static RESULT CCP_DUMP_init_(void);
+static RESULT TL_BCT_DIGEST_init_(void);
 
-static RESULT CCP_DUMP_cdis_(const CommandDispatcher* cdis);
-static RESULT CCP_DUMP_bct_(void);
-
-
-static CcpDump ccp_dump_;
-const CcpDump* const ccp_dump = &ccp_dump_;
+static RESULT TL_BCT_DIGEST_cdis_(const CommandDispatcher* cdis);
+static RESULT TL_BCT_DIGEST_bct_(void);
 
 
-AppInfo CCP_DUMP_create_app(void)
+static TlBctDigest tl_bct_digest_;
+const TlBctDigest* const tl_bct_digest = &tl_bct_digest_;
+
+
+AppInfo TL_BCT_DIGEST_create_app(void)
 {
-  return AI_create_app_info("ccpdump", CCP_DUMP_init_, NULL);
+  return AI_create_app_info("tl_bct_digest", TL_BCT_DIGEST_init_, NULL);
 }
 
 
-static RESULT CCP_DUMP_init_(void)
+static RESULT TL_BCT_DIGEST_init_(void)
 {
-  memset(&ccp_dump_, 0x00, sizeof(CcpDump));
-
-  // 初回はなにもダンプしてないので
-  ccp_dump_.dump.status = CCP_DUMP_STATUS_NOT_FOUND;
+  memset(&tl_bct_digest_, 0x00, sizeof(TlBctDigest));
   return RESULT_OK;
+}
+
+
+CCP_CmdRet Cmd_TL_BCT_DIGEST_TL(const CommonCmdPacket* packet)
+{
+  const PL_Node* node_head;
+  const PL_Node* node;
+  const PacketList* pl;
+  uint16_t i;
+  TL_BCT_DIGEST_TlDigest* tl_digest = &tl_bct_digest_.tl;
+  tl_digest->info.tl_id = (TLCD_ID)CCP_get_param_from_packet(packet, 0, uint8_t);
+  tl_digest->info.page_no = CCP_get_param_from_packet(packet, 1, uint8_t);
+
+  // 計算結果をクリア
+  memset(tl_digest->digests, 0x00, TL_BCT_DIGEST_TL_DIGEST_PAGE_SIZE * sizeof(uint16_t));
+
+  if (tl_digest->info.tl_id >= TLCD_ID_MAX)
+  {
+    tl_digest->info.status = TL_BCT_DIGEST_STATUS_PARAM_ERR;
+    return CCP_make_cmd_ret(CCP_EXEC_ILLEGAL_PARAMETER, 1);
+  }
+
+  pl = &(PH_tl_cmd_list[tl_digest->info.tl_id]);
+  tl_digest->info.queued = PL_count_active_nodes(pl);
+
+  if (tl_digest->info.queued <= tl_digest->info.page_no * TL_BCT_DIGEST_TL_DIGEST_PAGE_SIZE)
+  {
+    tl_digest->info.status = TL_BCT_DIGEST_STATUS_NO_CCP;
+    tl_digest->info.digests_num = 0;
+    tl_digest->info.time_stamp = TMGR_get_master_clock();
+    // たまたま計算するものがないだけなので， CCP_EXEC_SUCCESS を返す
+    return CCP_make_cmd_ret_without_err_code(CCP_EXEC_SUCCESS);
+  }
+
+  // 計算開始する node まで linked list をすすめる
+  node_head = PL_get_head(pl);
+
+  // for (i = 0; i < cdis_dump->queue_idx; ++i)
+  // {
+  //   node = PL_get_next(node);
+  // }
+  // if (node == NULL)
+  // {
+  //   return RESULT_ERR;
+  // }
+
+
+  // TODO
+
+
+  return CCP_make_cmd_ret_without_err_code(CCP_EXEC_SUCCESS);
+}
+
+
+CCP_CmdRet Cmd_TL_BCT_DIGEST_BCT(const CommonCmdPacket* packet)
+{
+  TL_BCT_DIGEST_BctDigest* bct_digest = &tl_bct_digest_.bct;
+  bct_digest->info.block = (bct_id_t)CCP_get_param_from_packet(packet, 0, uint16_t);
+
+  // 計算結果をクリア
+  memset(bct_digest->digests, 0x00, BCT_MAX_CMD_NUM * sizeof(uint16_t));
+
+  if (bct_digest->info.block >= BCT_MAX_BLOCKS)
+  {
+    bct_digest->info.status = TL_BCT_DIGEST_STATUS_PARAM_ERR;
+    return CCP_make_cmd_ret(CCP_EXEC_ILLEGAL_PARAMETER, 1);
+  }
+
+  bct_digest->info.digests_num = BCT_get_bc_length(bct_digest->info.block);
+
+
+  // TODO
+
+
+  return CCP_make_cmd_ret_without_err_code(CCP_EXEC_SUCCESS);
 }
 
 #pragma section
--- applications/tl_bct_digest.h
+++ applications/tl_bct_digest.h
@@ -18,6 +18,18 @@
 
 
 /**
+ * @enum   TL_BCT_DIGEST_STATUS
+ * @note   uint8_t を想定
+ * @brief  digest 計算結果
+ */
+typedef enum
+{
+  TL_BCT_DIGEST_STATUS_OK,         //!< 計算成功
+  TL_BCT_DIGEST_STATUS_PARAM_ERR,  //!< パラメタエラー
+  TL_BCT_DIGEST_STATUS_NO_CCP      //!< digest を計算すべき CCP はなし
+} TL_BCT_DIGEST_STATUS;
+
+/**
  * @struct TL_BCT_DIGEST_TlDigest
  * @brief  TL の digest
  */
@@ -25,10 +37,12 @@ typedef struct
 {
   struct
   {
-    TLCD_ID tl_id;            //!< TL の ID
-    uint8_t page_no;          //!< TL_BCT_DIGEST_TL_DIGEST_PAGE_SIZE のページネーション
-    ObcTime time_stamp;       //!< digest 計算時刻
-    uint8_t digests_num;      //!< 計算した digests の数
+    TLCD_ID tl_id;                //!< TL の ID
+    uint8_t page_no;              //!< TL_BCT_DIGEST_TL_DIGEST_PAGE_SIZE のページネーション
+    TL_BCT_DIGEST_STATUS status;  //!< digest 計算結果
+    uint16_t queued;              //!< 該当の TL に保存されたコマンド数
+    ObcTime time_stamp;           //!< digest 計算時刻
+    uint8_t digests_num;          //!< 計算した digests の数
   } info;
   uint16_t digests[TL_BCT_DIGEST_TL_DIGEST_PAGE_SIZE];  //!< TL の digest
 } TL_BCT_DIGEST_TlDigest;
@@ -41,9 +55,10 @@ typedef struct
 {
   struct
   {
-    bct_id_t block;           //!< BCT block
-    ObcTime time_stamp;       //!< digest 計算時刻
-    uint8_t digests_num;      //!< 計算した digests の数
+    bct_id_t block;               //!< BCT block
+    TL_BCT_DIGEST_STATUS status;  //!< digest 計算結果
+    ObcTime time_stamp;           //!< digest 計算時刻
+    uint8_t digests_num;          //!< 計算した digests の数（＝ BCT に登録されたコマンド数）
   } info;
   uint16_t digests[BCT_MAX_CMD_NUM];  //!< BCT の digest
 } TL_BCT_DIGEST_BctDigest;
