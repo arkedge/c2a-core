9399f62ffc889263cbd223dba94adf941b27f0b9
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2024-01-01 18:11:26 +0900

    Update test_component_driver_utility.py

--- examples/mobc/src/src_user/test/src_core/applications/test_component_driver_utility.py
+++ examples/mobc/src/src_user/test/src_core/applications/test_component_driver_utility.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-
-import isslwings as wings
-import pytest
-
-ROOT_PATH = "../../../"
-sys.path.append(os.path.dirname(__file__) + "/" + ROOT_PATH + "utils")
-import c2a_enum_utils
-import wings_utils
-
-c2a_enum = c2a_enum_utils.get_c2a_enum()
-ope = wings_utils.get_wings_operation()
+# import os
+# import sys
+#
+# import isslwings as wings
+# import pytest
+#
+# ROOT_PATH = "../../../"
+# sys.path.append(os.path.dirname(__file__) + "/" + ROOT_PATH + "utils")
+# import c2a_enum_utils
+# import wings_utils
+#
+# c2a_enum = c2a_enum_utils.get_c2a_enum()
+# ope = wings_utils.get_wings_operation()
 
 # TBA
 # subobc が必要なテストは examples/subobc/src/src_user/test/test/test_component_driver_utility.py に配置した
