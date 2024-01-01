c4e79fa37d2f94e820961ba885b40af11e7e9f80
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2024-01-01 17:45:17 +0900

    add hal low level cmd

---
+++ examples/mobc/src/src_user/settings/applications/component_driver_utility_params.h
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * @brief  ComponentDriverUtility の CoreApp パラメタのオーバーライド用ヘッダー
+ */
+#ifndef COMPONENT_DRIVER_UTILITY_PARAMS_H_
+#define COMPONENT_DRIVER_UTILITY_PARAMS_H_
+
+#undef CDRV_UTIL_HAX_RX_BUFFER_SIZE
+
+#define CDRV_UTIL_HAX_RX_BUFFER_SIZE    (256)
+
+#endif
