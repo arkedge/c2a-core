7547ef6539b70aa641f136c04bf46d1ef2e1a0d0
ToshiAki64 <akima-toshifumi@arkedgespace.com>
2024-03-11 15:16:55 +0900

    revise TI for flash_screen

--- examples/subobc/src/src_user/settings/modes/task_lists/elements/tl_elem_debug_display.c
+++ examples/subobc/src/src_user/settings/modes/task_lists/elements/tl_elem_debug_display.c
@@ -14,10 +14,10 @@ void BCL_load_tl_elem_debug_display_initial(void)
   BCL_tool_register_app    ( 3, AR_APP_DBG_PRINT_EVENT_LOGGER0);
   BCL_tool_register_app    ( 4, AR_APP_DBG_PRINT_EVENT_LOGGER1);
   BCL_tool_register_app    ( 5, AR_APP_DBG_PRINT_EVENT_HANDLER);
-  BCL_tool_register_app    ( 6, AR_APP_DBG_FLUSH_SCREEN);
+  BCL_tool_register_app    ( 6, AR_NOP);
   BCL_tool_register_app    ( 7, AR_NOP);
   BCL_tool_register_app    ( 8, AR_NOP);
-  BCL_tool_register_app    ( 9, AR_NOP);
+  BCL_tool_register_app    ( 9, AR_APP_DBG_FLUSH_SCREEN);
 }
 
 #pragma section
