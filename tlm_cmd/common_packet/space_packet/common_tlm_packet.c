37bad3a3a5178869d1bed520dc9f09f40a3b1337
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2024-04-03 18:03:48 +0900

    add C2A_USE_DEFAULT_GLOBAL_TIME_IN_CTP

--- tlm_cmd/common_packet/space_packet/common_tlm_packet.c
+++ tlm_cmd/common_packet/space_packet/common_tlm_packet.c
@@ -33,12 +33,15 @@ double CTP_get_global_time(const CommonTlmPacket* packet)
   return TSP_get_global_time(packet);
 }
 
+#ifdef C2A_USE_DEFAULT_GLOBAL_TIME_IN_CTP
 void CTP_set_global_time(CommonTlmPacket* packet)
 {
   // 何を設定するかはユーザー定義
   // TMGR_get_curret_unixtime() で現在の unixtime を入れたり, gps 時刻 を入れたり, など
+  // User 独自の値を設定する場合は， C2A_USE_DEFAULT_GLOBAL_TIME_IN_CTP を OFF にすること
   TSP_set_global_time(packet, 0.0);
 }
+#endif
 
 uint32_t CTP_get_on_board_subnet_time(const CommonTlmPacket* packet)
 {
