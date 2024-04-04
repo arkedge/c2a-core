dd901bd5e956d017f157b3aecc8aefddee9a0ca1
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2024-04-04 21:44:38 +0900

    divide CTP_set_global_time

---
+++ examples/mobc/src/src_user/settings/tlm_cmd/common_tlm_packet_define.c
@@ -0,0 +1,16 @@
+#pragma section REPRO
+/**
+ * @file
+ * @brief  CTP 型の user 定義
+ */
+#include <src_core/tlm_cmd/common_packet/common_tlm_packet.h>
+
+void CTP_set_global_time(CommonTlmPacket* packet)
+{
+  // 何を設定するかはユーザー定義
+  // TMGR_get_curret_unixtime() で現在の unixtime を入れたり, gps 時刻 を入れたり, など
+  // User 独自の値を設定する場合は， C2A_USE_DEFAULT_GLOBAL_TIME_IN_CTP を OFF にすること
+  TSP_set_global_time(packet, 0.0);
+}
+
+#pragma section
